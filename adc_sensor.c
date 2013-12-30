f729a1eed4419f46df0e0ea2842ee00a3341e5a7
Ben Gamari <bgamari.foss@gmail.com>
2013-12-30 11:06:43 -0500

    Add adc sensor queue
    ADC access needs to be serialized

--- adc_sensor.c
+++ adc_sensor.c
@@ -1,4 +1,11 @@
+#include <stdbool.h>
+#include "config.h"
 #include "adc_sensor.h"
+
+static bool adc_busy = false;
+static struct sample_queue *queue_head;
+
+static void adc_schedule(void);
 
 static void
 adc_sample_done(uint16_t codepoint, int error, void* cbdata)
@@ -7,11 +14,36 @@ adc_sample_done(uint16_t codepoint, int error, void* cbdata)
         struct adc_sensor_data *sensor_data = sensor->sensor_data;
         accum value = (*sensor_data->map)(codepoint, sensor_data->map_data);
         sensor_new_sample(sensor, value);
+        adc_busy = false;
+        adc_schedule();
+}
+
+// ADC requests need to be serialized. This is where we do this.
+static void
+adc_schedule()
+{
+        if (adc_busy)
+                return;
+
+        crit_enter();
+        if (queue_head) {
+                struct adc_sensor_data *sensor_data = queue_head->sensor->sensor_data;
+                adc_busy = true;
+                adc_sample_start(sensor_data->channel, adc_sample_done, queue_head->sensor);
+                queue_head = queue_head->next;
+        }
+        crit_exit();
 }
 
 void
 adc_sensor_sample(struct sensor *sensor)
 {
         struct adc_sensor_data *sensor_data = sensor->sensor_data;
-        adc_sample_start(sensor_data->channel, adc_sample_done, sensor);
+        crit_enter();
+        sensor_data->queue.sensor = sensor;
+        sensor_data->queue.next = queue_head;
+        queue_head = &sensor_data->queue;
+        crit_exit();
+
+        adc_schedule();
 }
--- adc_sensor.h
+++ adc_sensor.h
@@ -4,10 +4,18 @@
 
 typedef accum (*adc_map_func)(uint16_t voltage, void *map_data);
 
+struct sample_queue {
+        struct sensor *sensor;
+        struct sample_queue *next;
+};
+
 struct adc_sensor_data {
         enum adc_channel channel;
         adc_map_func map;
         void *map_data;
+
+        // internal
+        struct sample_queue queue;
 };
 
 void adc_sensor_sample(struct sensor *sensor);

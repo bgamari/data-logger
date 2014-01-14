952509687f7a8831a4d62a8821aa31a3cb3a6f08
Ben Gamari <bgamari.foss@gmail.com>
2014-01-14 12:23:43 -0500

    radio: Have live-streaming working

--- radio.c
+++ radio.c
@@ -8,12 +8,13 @@ static unsigned int dropped_samples = 0;
 
 struct nrf_sample {
         uint32_t time;
-        uint8_t sensor_id;
-        accum value;
+        uint16_t node_id;
+        uint16_t sensor_id;
+        accum    value;
 };
 
-#define NBUFFERS 2
-#define BUFFER_LEN 16
+#define NBUFFERS 16
+#define BUFFER_LEN (32 / sizeof(struct nrf_sample))
 
 struct nrf_buffer {
         struct nrf_sample buffer[BUFFER_LEN];
@@ -21,48 +22,51 @@ struct nrf_buffer {
         struct nrf_buffer *next;
 };
 static struct nrf_buffer radio_buffers[NBUFFERS];
-static struct nrf_buffer *fill_queue;
-static struct nrf_buffer *send_queue;
+static struct nrf_buffer *fill_queue = NULL;
+static struct nrf_buffer *send_queue = NULL;
 
 struct nrf_addr_t dest_addr = {
 	.value =  0xf0f0f0f0e1ll,
 	.size = 5
 };
 
-void
-radio_init()
-{
-        for (unsigned int i=0; i<NBUFFERS; i++)
-                radio_buffers[i].next = &radio_buffers[i+1];
-        radio_buffers[NBUFFERS-1].next = NULL;
-        fill_queue = &radio_buffers[0];
-                        
-        nrf_init();
-	nrf_set_autoretransmit(3, 5); // 1000us, 5x
-	nrf_set_channel(16);
-	nrf_set_power_datarate(NRF_TX_POWER_0DBM, NRF_DATA_RATE_1MBPS);
-	nrf_enable_dynamic_payload();
-	nrf_enable_powersave();
-}
-
 static void radio_send();
 
-static void
-radio_sample_sent(void *payload, uint8_t bytes_sent)
+// append buf to end of given queue
+// returns true if queue was previously empty
+static bool
+append_to_queue(struct nrf_buffer **head, struct nrf_buffer *buf)
 {
+        if (*head) {
+                struct nrf_buffer *tail = *head;
+                while (tail->next != NULL)
+                        tail = tail->next;
+
+                buf->next = NULL;
+                tail->next = buf;
+                return false;
+        } else {
+                buf->next = NULL;
+                *head = buf;
+                return true;
+        }
+}
+
+static void
+radio_buffer_sent(void *payload, uint8_t bytes_sent)
+{
+        if (bytes_sent < sizeof(struct nrf_sample) * send_queue->head) {
+                radio_send();
+                return;
+        }
+
         crit_enter();
         struct nrf_buffer *sent = send_queue;
-        send_queue = send_queue->next;
-
-        // find tail of fill queue
-        struct nrf_buffer *tail = fill_queue;
-        while (tail->next != NULL)
-                tail = tail->next;
+        send_queue = sent->next;
 
         // place buffer on end of fill queue
-        sent->next = NULL;
         sent->head = 0;
-        tail->next = sent;
+        append_to_queue(&fill_queue, sent);
 
         // send next
         radio_send();
@@ -76,7 +80,7 @@ radio_send()
         if (send_queue) {
                 nrf_send(&dest_addr, send_queue->buffer,
                          sizeof(struct nrf_sample) * send_queue->head,
-                         radio_sample_sent);
+                         radio_buffer_sent);
         }
 }
         
@@ -88,24 +92,17 @@ radio_finish_buffer()
         struct nrf_buffer *done = fill_queue;
         fill_queue = fill_queue->next;
 
-        if (send_queue) {
-                // find tail of send queue
-                struct nrf_buffer *tail = send_queue;
-                while (tail->next != NULL)
-                        tail = tail->next;
-
-                done->next = NULL;
-                tail->next = done;
-        } else {
-                send_queue = done;
+        if (append_to_queue(&send_queue, done))
                 radio_send();
-        }
         crit_exit();
 }
 
 static void
 radio_new_sample(struct sensor *sensor, accum value, void *cbdata)
 {
+        if (!radio_enabled)
+                return;
+
         crit_enter();
         if (!fill_queue) {
                 dropped_samples++;
@@ -115,13 +112,15 @@ radio_new_sample(struct sensor *sensor, accum value, void *cbdata)
 
         fill_queue->buffer[fill_queue->head] = (struct nrf_sample) {
                 .time = rtc_get_time(),
+                .node_id = 0x1, // FIXME
                 .sensor_id = sensor->sensor_id,
                 .value = value
         };
-        fill_queue->head++;
 
-        if (fill_queue->head == BUFFER_LEN) {
+        if (fill_queue->head == BUFFER_LEN - 1) {
                 radio_finish_buffer();
+        } else {
+                fill_queue->head++;
         }
         crit_exit();
 }
@@ -129,10 +128,6 @@ radio_new_sample(struct sensor *sensor, accum value, void *cbdata)
 void
 radio_enable()
 {
-        if (radio_enabled)
-                return;
-
-        sensor_listen(&radio_listener, radio_new_sample, NULL);
         radio_enabled = true;
 }
 
@@ -146,4 +141,24 @@ bool
 radio_get_enabled()
 {
         return radio_enabled;
+}
+
+void
+radio_init()
+{
+        for (unsigned int i=0; i<NBUFFERS; i++) {
+                radio_buffers[i].head = 0;
+                radio_buffers[i].next = &radio_buffers[i+1];
+        }
+        radio_buffers[NBUFFERS-1].next = NULL;
+        fill_queue = &radio_buffers[0];
+                        
+        nrf_init();
+	nrf_set_autoretransmit(3, 5); // 1000us, 5x
+	nrf_set_channel(16);
+	nrf_set_power_datarate(NRF_TX_POWER_0DBM, NRF_DATA_RATE_1MBPS);
+	nrf_enable_dynamic_payload();
+	nrf_enable_powersave();
+
+        sensor_listen(&radio_listener, radio_new_sample, NULL);
 }

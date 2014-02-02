5414579bd9713db13a7099df5f69cd9e2e0c40d8
Ben Gamari <bgamari.foss@gmail.com>
2014-02-02 09:34:15 -0500

    conductivity: Un-hard-code FTM channels

--- conductivity.c
+++ conductivity.c
@@ -4,6 +4,8 @@
 
 static struct cond_sample_ctx *head = NULL;
 
+#define BASE_CH 6
+
 static void
 cond_start(void)
 {
@@ -14,7 +16,7 @@ cond_start(void)
 
         // start continuous dual-edge capture mode
         FTM0.sc.clks = 1; // enable clock
-        FTM0.combine[0].decap = 1;
+        FTM0.combine[BASE_CH/2].decap = 1;
 }
 
 static void
@@ -84,23 +86,23 @@ cond_init(void)
         FTM0.sc.ps = FTM_PS_DIV32; // prescale
         FTM0.mode.ftmen = 1;
         int_enable(IRQ_FTM0);
-        FTM0.combine[0].decapen = 1;
+        FTM0.combine[BASE_CH/2].decapen = 1;
 
         // channel 0 captures falling edge
-        FTM0.channel[0].csc.msa = 1;  // continuous
-        FTM0.channel[0].csc.elsa = 0;
-        FTM0.channel[0].csc.elsb = 1;
-        FTM0.channel[0].csc.chf = 0;
+        FTM0.channel[BASE_CH].csc.msa = 1;  // continuous
+        FTM0.channel[BASE_CH].csc.elsa = 0;
+        FTM0.channel[BASE_CH].csc.elsb = 1;
+        FTM0.channel[BASE_CH].csc.chf = 0;
 
         // channel 1 captures rising edge
-        FTM0.channel[1].csc.elsa = 0;
-        FTM0.channel[1].csc.elsb = 1;
-        FTM0.channel[1].csc.chf = 0;
-        FTM0.channel[1].csc.chie = 1;
+        FTM0.channel[BASE_CH+1].csc.elsa = 0;
+        FTM0.channel[BASE_CH+1].csc.elsb = 1;
+        FTM0.channel[BASE_CH+1].csc.chf = 0;
+        FTM0.channel[BASE_CH+1].csc.chie = 1;
 
-        // PTC1 = pin 34 = FTM0_CH0
-        FTM0.filter.ch0fval = 5;
-        pin_mode(PIN_PTC1, PIN_MODE_MUX_ALT4); // | PIN_MODE_PULLDOWN);
+        // PTA1 = FTM0_CH7
+        // FIXME
+        //FTM0.filter.ch6fval = 5;
 }
 
 #define UNUSED(expr) do { (void)(expr); } while (0)
@@ -111,14 +113,14 @@ FTM0_Handler(void)
         // acknowledge interrupt
         uint32_t a;
         UNUSED(a);
-        a = FTM0.channel[0].csc.raw;
-        FTM0.channel[0].csc.chf = 0;
-        a = FTM0.channel[1].csc.raw;
-        FTM0.channel[1].csc.chf = 0;
+        a = FTM0.channel[BASE_CH].csc.raw;
+        FTM0.channel[BASE_CH].csc.chf = 0;
+        a = FTM0.channel[BASE_CH+1].csc.raw;
+        FTM0.channel[BASE_CH+1].csc.chf = 0;
 
         // fetch edge times
-        uint32_t t1 = FTM0.channel[0].cv;
-        uint32_t t2 = FTM0.channel[1].cv;
+        uint32_t t1 = FTM0.channel[BASE_CH].cv;
+        uint32_t t2 = FTM0.channel[BASE_CH+1].cv;
 
         int32_t dt = t2 - t1;
         if (dt < 0) dt += 0xffff;

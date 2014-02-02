bfc2942957dc2ed4ae6364f3e031accf7fbbb328
Ben Gamari <bgamari.foss@gmail.com>
2014-02-02 09:35:00 -0500

    Add LM19 map

--- temperature.c
+++ temperature.c
@@ -1,4 +1,5 @@
 #include "temperature.h"
+#include <math.h>
 
 static accum
 temperature_map(uint16_t codepoint, void *map_data)
@@ -15,3 +16,9 @@ struct adc_sensor_data temperature_sensor_data = {
         .map = temperature_map
 };
                  
+accum
+lm19_map(uint16_t codepoint, void *map_data)
+{
+        float v = 3.3 * codepoint / 0xffff;
+        return -1481.96 + sqrt(2.1962e6 + (1.8639 - v) / 3.88e-6);
+}
--- temperature.h
+++ temperature.h
@@ -2,3 +2,5 @@
 #include "adc_sensor.h"
 
 extern struct adc_sensor_data temperature_sensor_data;
+
+accum lm19_map(uint16_t codepoint, void *map_data);

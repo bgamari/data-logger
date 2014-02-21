4398d2efd4edf21cef22ff2cb5e670d9353a9a88
Ben Gamari <bgamari.foss@gmail.com>
2014-02-21 14:08:42 -0500

    flash_list: Add Winbond W25Q64

--- flash_list.c
+++ flash_list.c
@@ -3,7 +3,8 @@
 struct spiflash_params spiflash_device_params[] = {
         { 0xbf, 0x25, 0x8e,  BLOCK_SIZE_64KB,   16,  {.raw = 0x00} }, // SST25VF080B
         { 0x1f, 0x48, 0x00,  BLOCK_SIZE_64KB,  128,  {.raw = 0x00} }, // Adesto AT25DF641A
-        { 0xef, 0x40, 0x14,  BLOCK_SIZE_64KB,   16,  {.raw = 0x00} }, // Winbond W25Q80BV
+        { 0xef, 0x40, 0x14,  BLOCK_SIZE_64KB,   16,  {.raw = 0x00} }, // Winbond W25Q80
+        { 0xef, 0x40, 0x17,  BLOCK_SIZE_64KB,  128,  {.raw = 0x00} }, // Winbond W25Q64
         { }
 };
         

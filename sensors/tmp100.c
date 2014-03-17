c53bf4f585fce823c93c94614639aec6f95f9666
Ben Gamari <bgamari.foss@gmail.com>
2014-03-17 11:13:13 -0400

    tmp100: Catch i2c NACK

--- sensors/tmp100.c
+++ sensors/tmp100.c
@@ -27,12 +27,17 @@ tmp100_init(struct tmp100_ctx *ctx, uint8_t address, i2c_cb *cb, void *cbdata)
 static void
 tmp100_sample_done(uint8_t *buf, enum i2c_result result, void *cbdata)
 {
+        struct sensor *sensor = cbdata;
+        if (result != I2C_RESULT_SUCCESS) {
+                sensor_sample_failed(sensor);
+                return;
+        }
+
         uint16_t word = (buf[0] << 4) | (buf[1] >> 4);
         int16_t temp = word & ~(1 << 11);
         if (word & (1<<11))
                 temp = 0x1000 - temp;
         accum real_temp = 1. * temp / 16;
-        struct sensor *sensor = cbdata;
         sensor_new_sample(sensor, 0, real_temp);
 }
 
@@ -41,6 +46,10 @@ tmp100_sample_start(enum i2c_result result, struct i2c_transaction *trans)
 {
         struct sensor *sensor = trans->cbdata;
         struct tmp100_sensor_data *sd = sensor->sensor_data;
+        if (result != I2C_RESULT_SUCCESS) {
+                sensor_sample_failed(sensor);
+                return;
+        }
         i2c_reg_read(&sd->reg_read, sd->tmp100.trans.address,
                      TMP100_REG_TEMPERATURE, sd->buf, 2,
                      tmp100_sample_done, sensor);

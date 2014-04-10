24cfdde08311eeb942da11fbd81171116b7be057
Ben Gamari <bgamari.foss@gmail.com>
2014-04-10 12:11:50 -0400

    conductivity_gpio: Almost working

--- sensors/conductivity_gpio.c
+++ sensors/conductivity_gpio.c
@@ -9,22 +9,33 @@ void
 cond_gpio_init(struct cond_gpio_sensor_data *cond)
 {
         pin_mode(cond->pin_a, PIN_MODE_MUX_GPIO);
-        pin_mode(cond->pin_b, PIN_MODE_MUX_GPIO);
         gpio_dir(cond->pin_a, GPIO_OUTPUT);
-        gpio_dir(cond->pin_b, GPIO_OUTPUT);
         gpio_write(cond->pin_a, 0);
+
+        pin_mode(cond->pin_b, PIN_MODE_MUX_GPIO);
+        gpio_dir(cond->pin_b, GPIO_OUTPUT);
         gpio_write(cond->pin_b, 0);
 
-        pin_mode(PIN_PTC2, PIN_MODE_MUX_ANALOG);
+        pin_mode(PIN_PTC2, PIN_MODE_MUX_GPIO);
         SIM.scgc4.cmp = 1;
+        int_enable(IRQ_CMP1);
 }
 
 void
 cond_gpio_start(struct sensor *sensor)
 {
         struct cond_gpio_sensor_data *sd = sensor->sensor_data;
-        CMP1.daccr.vosel = (phase + 1) * 64 / 3;
-        CMP1.cr1.inv = sd->phase;
+        if (sd->phase) {
+                // waiting for rising edge
+                CMP1.scr.ief = false;
+                CMP1.scr.ier = true;
+        } else {
+                // waiting for falling edge
+                CMP1.scr.ier = false;
+                CMP1.scr.ief = true;
+        }
+
+        CMP1.daccr.vosel = (sd->phase + 1) * 64 / 3;
         gpio_write(sd->pin_a, sd->phase);
         gpio_write(sd->pin_b, !sd->phase);
 }
@@ -35,22 +46,22 @@ cond_gpio_sample_done(struct sensor *sensor)
 
         struct cond_gpio_sensor_data *sd = sensor->sensor_data;
         uint32_t dt = timeout_get_time().time - sd->start_time.time;
-        accum dt_accum = dt / 1000;
+        accum dt_accum = 1.0 * dt / sd->transitions;
 
         CMP1.cr1.en = 0;
         CMP1.daccr.dacen = 0;
         timeout_put_ref();
-        gpio_dir(sd->pin_a, GPIO_DISABLE);
-        gpio_dir(sd->pin_b, GPIO_DISABLE);
+        gpio_write(sd->pin_a, 0);
+        gpio_write(sd->pin_b, 0);
 
         sensor_new_sample(sensor, &dt_accum);
 }
 
 void
-CMP_Handler(void)
+CMP1_Handler(void)
 {
         struct cond_gpio_sensor_data *sd = _cond_sensor->sensor_data;
-        CMP1.scr.cfr = 1;
+        CMP1.scr.raw |= ((struct CMP_SCR_t) {.cfr = 1, .cff = 1}).raw;
         sd->phase = !sd->phase;
         sd->count--;
         if (sd->count > 0)
@@ -67,13 +78,16 @@ cond_gpio_sample(struct sensor *sensor)
         timeout_get_ref();
         sd->start_time = timeout_get_time();
         sd->count = sd->transitions;
+        sd->phase = true;
 
         CMP1.daccr.dacen = 1;
         CMP1.daccr.vrsel = 1;
-        CMP1.cr1.pmode = 0;
-        CMP1.muxcr.psel = 0x0;
-        CMP1.muxcr.msel = 0x7;
-        CMP1.cr1.en = 1;
+        CMP1.muxcr.raw = ((struct CMP_MUXCR_t) {.psel = 0x0, .msel = 0x7}).raw;
+        CMP1.cr1.raw = ((struct CMP_CR1_t) {
+                        .en = 1,
+                        .pmode = 0,
+                        .ope = 0,
+                        }).raw;
         CMP1.scr.ier = 1;
 
         cond_gpio_start(sensor);
